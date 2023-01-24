7cf52c1aca7f82721c52842ea76ce8805e7300da
Daniel Markow <daniel.markow@outlook.com>
2023-01-24 03:56:53 +0100

    static paths

--- src/pages/[restaurantId].tsx
+++ src/pages/[restaurantId].tsx
@@ -2,39 +2,56 @@ import { useRouter } from "next/router";
 import { api } from "../utils/api";
 
 import { createProxySSGHelpers } from "@trpc/react-query/ssg";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import {
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+  GetStaticPaths,
+} from "next";
 import superjson from "superjson";
 
 import { appRouter } from "../server/api/root";
-import { createContext } from "../server/api/trpc";
+import { prisma } from "../server/db";
 
-export async function getServerSideProps(
-  context: GetServerSidePropsContext<{ restaurantId: string }>
+export async function getStaticProps(
+  context: GetStaticPropsContext<{ restaurantId: string }>
 ) {
-  const ssg = createProxySSGHelpers({
+  const ssg = await createProxySSGHelpers({
     router: appRouter,
-    ctx: await createContext(),
+    ctx: {},
     transformer: superjson,
   });
-
   const restaurantId = context.params?.restaurantId as string;
-
+  // prefetch `post.byId`
   await ssg.dishes.getDishes.prefetch({ restaurantId });
-
   return {
     props: {
       trpcState: ssg.dehydrate(),
       restaurantId,
     },
+    revalidate: 1,
   };
 }
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  const dishes = await prisma.dish.findMany({
+    select: {
+      restaurantId: true,
+    },
+  });
+  return {
+    paths: dishes.map((dish) => ({
+      params: {
+        restaurantId: dish.restaurantId,
+      },
+    })),
+    // https://nextjs.org/docs/basic-features/data-fetching#fallback-blocking
+    fallback: "true",
+  };
+};
+
 export default function DishesList(
-  props: InferGetServerSidePropsType<typeof getServerSideProps>
+  props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
-  console.log(props);
-  // const router = useRouter();
-  // const { restaurantId } = router.query;
   const { restaurantId } = props;
   const dishes = api.dishes.getDishes.useQuery({ restaurantId });
 
